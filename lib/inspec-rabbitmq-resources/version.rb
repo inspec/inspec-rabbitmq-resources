5843d0953dfe9cb0da5388e44ab8d884c19e3ecf
Sonu Saha <sonu.saha@progress.com>
2025-03-27 18:48:57 +0530

    chore: sync version with InSpec major version
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- lib/inspec-rabbitmq-resources/version.rb
+++ lib/inspec-rabbitmq-resources/version.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
-
 # This file simply makes it easier for CI engines to update
 # the version stamp, and provide a clean way for the gemspec
 # to learn the current version.
 module InspecPlugins
   module RabbitmqResources
-    VERSION = "0.1.6"
+    VERSION = "7.0.0"
   end
 end
